ae7a447324cfe6d871d2068f7cd5228716f130b8
Jonas Charrier <jonas.charrier1@gmail.com>
2022-10-31 09:39:42 +0100

    implementing open_file_dialog on android

--- src/android/src/toga_android/dialogs.py
+++ src/android/src/toga_android/dialogs.py
@@ -1,8 +1,37 @@
 import asyncio
 
+# from com.chaquo.python import Python
+from java import Override, jint, jvoid, static_proxy
+
+from android.app import Activity
+from android.content import Intent  # Context
+from android.net import Uri
+
 from .libs.android import R__drawable
 from .libs.android.app import AlertDialog__Builder
 from .libs.android.content import DialogInterface__OnClickListener
+
+
+class FileDialogsActivity(static_proxy(Activity)):
+    def __init__(self):
+        super().__init__()
+
+    @Override(jvoid, [jint, Intent])
+    def onActivityResult(self, requestCode, resultCode, returnIntent):
+        selected_uri = None
+        if resultCode == Activity.RESULT_OK:
+            if returnIntent is not None:
+                selected_uri = returnIntent.getData()
+                if selected_uri is None:
+                    selected_uri = []
+                    clip_data = returnIntent.getClipData()
+                    if clip_data is not None:
+                        for i in range(0, clip_data.getItemCount()):
+                            selected_uri.append(str(clip_data.getItemAt(i).getUri()))
+                else:
+                    selected_uri = [str(selected_uri)]
+        if selected_uri is None:
+            raise ValueError("No filename provided in the open file dialog")
 
 
 class OnClickListener(DialogInterface__OnClickListener):
@@ -33,14 +62,14 @@ class BaseDialog:
 
 class TextDialog(BaseDialog):
     def __init__(
-        self,
-        window,
-        title,
-        message,
-        positive_text,
-        negative_text=None,
-        icon=None,
-        on_result=None,
+            self,
+            window,
+            title,
+            message,
+            positive_text,
+            negative_text=None,
+            icon=None,
+            on_result=None,
     ):
         """Create Android textual dialog.
 
@@ -136,7 +165,31 @@ class SaveFileDialog(BaseDialog):
 class OpenFileDialog(BaseDialog):
     def __init__(self, window, title, initial_directory, file_types, multiselect, on_result=None):
         super().__init__()
-        window.factory.not_implemented("Window.open_file_dialog()")
+        self.on_result = on_result
+        self.multiselect = multiselect
+
+        intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.setType("*/*")
+        if initial_directory is not None and str(initial_directory) != '':
+            intent.putExtra("android.provider.extra.INITIAL_URI", Uri.parse(str(initial_directory)))
+        if file_types is not None and file_types != ['']:
+            # Commented out because rubicon currently does not support arrays and nothing else works with this Intent
+            # see https://github.com/beeware/rubicon-java/pull/53
+            # intent.putExtra(Intent.EXTRA_MIME_TYPES, file_mime_types)
+            window.factory.not_implemented(
+                'Window.open_file_dialog() on Android currently does not support the file_type parameter')
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, multiselect)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        # context = Python.getPlatform().getApplication()
+        context = FileDialogsActivity()
+        context.startActivityForResult(intent, jint(1))
+
+    def completion_handler(self, return_value: bool) -> None:
+        if self.on_result:
+            self.on_result(self, return_value)
+
+        self.future.set_result(return_value)
 
 
 class SelectFolderDialog(BaseDialog):
